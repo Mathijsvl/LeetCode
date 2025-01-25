f6bfd3a0f2736b66c0715d170146c34cb174caeb
Mathijs van Lith <mathijsvanlith@outlook.com>
2025-01-25 22:42:04 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,34 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        merged_string = []
+        i, j = 0, 0
+        
+        while i < len(word1) and j < len(word2):
+            merged_string.append(word1[i])
+            merged_string.append(word2[j])
+            i += 1
+            j += 1
+        
+        # Append remaining characters from the longer string
+        if i < len(word1):
+            merged_string.append(word1[i:])
+        if j < len(word2):
+            merged_string.append(word2[j:])
+        
+        return ''.join(merged_string)
+
+# Create an instance of the Solution class
+solution = Solution()
+
+# Testcases
+word1 = "abc"
+word2 = "pqr"
+print(solution.mergeAlternately(word1, word2))  # Output: "apbqcr"
+
+word1 = "ab"
+word2 = "pqrs"
+print(solution.mergeAlternately(word1, word2))  # Output: "apbqrs"
+
+word1 = "abcd"
+word2 = "pq"
+print(solution.mergeAlternately(word1, word2))  # Output: "apbqcd"
